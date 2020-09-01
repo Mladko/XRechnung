285e7642f4cd82663d49593f179fd27a64a55083
Sikiric <Sikiric@NB0110.HPM.local>
2020-09-01 14:45:00 +0200

    Kardinalitäten ergänzt. Typen der einzelnen Attribute müssen noch überprüft werden

---
+++ XRechnung/src/main/java/com/hoecker/xrechnung/constants/InvoiceTypeCode.java
@@ -0,0 +1,22 @@
+/*
+ * Mladen Sikiric
+ * Höcker Project Managers GmbH
+ */
+package com.hoecker.xrechnung.constants;
+
+/**
+ *
+ * @author Sikiric
+ */
+public class InvoiceTypeCode {
+
+    public static final int PARTIAL_INVOICE = 326;
+    public static final int COMMERCIAL_INVOICE = 380;
+    public static final int CORRECTED_INVOICE = 384;
+    public static final int SELF_BILLED_INVOICE = 389;
+    public static final int CREDIT_NOTE = 381;
+    public static final int PARTIAL_CONSTRUCTION_INVOICE = 875;
+    public static final int PARTIAL_FINAL_INVOICE = 876;
+    public static final int FINAL_CONSTRUCTION_INVOICE = 877;
+    
+}
---
+++ XRechnung/src/main/java/com/hoecker/xrechnung/constants/ValueAddedTaxPointDateCode.java
@@ -0,0 +1,17 @@
+/*
+ * Mladen Sikiric
+ * Höcker Project Managers GmbH
+ */
+package com.hoecker.xrechnung.constants;
+
+/**
+ *
+ * @author Sikiric
+ */
+public class ValueAddedTaxPointDateCode {
+
+    public static final int INVOICE_DOCUMENT_ISSUE_DATE_TIME = 3;
+    public static final int DELIVERY_DATE_TIME_ACUAL = 35;
+    public static final int PAID_TO_DATE = 432;
+
+}
